a4d82791bed1be4a3a3ec9cf2539126faf3c2d5d
rahulthevampire <91219499+rahulthevampire@users.noreply.github.com>
2021-09-23 01:09:47 +0530

    foodrestorent

---
+++ food res/htdocs/rahul/switchcase.php
@@ -0,0 +1,8 @@
+<?php
+$age = 34;
+switch($age){
+  case 34:
+    echo "you are 34 year old";
+    case 12:
+        echo "you are 12 year old";
+}
---
+++ food res/htdocs/rahul/t2.php
@@ -0,0 +1,20 @@
+<?php
+// variables arwe the cotainer to store inrmation
+//it is start with $ and it is case sensitive.
+//$name ="rahul";
+////$income ="233";
+//echo " this $name got a job and earn daily $income <br> ";
+//echo " $name is good boy";
+
+//$N ="GIGDETH";
+//$B = TRUE;
+//$fRIENDS = ARRAY("RAHUL","POLLA","AKIUII","WETRG");
+//echo var_dump($fRIENDS);
+//echo $fRIENDS[2];
+echo "welcome we are learning how we et cookies"
+//syntax to set cookies
+
+setcookie("category", "books",time()+ 86400,"/");
+
+
+?>
